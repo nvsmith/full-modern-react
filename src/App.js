be1509cb8ba040b3450a2ce4150987aa47ca4cea
Nathan Smith <nathanielvs@gmail.com>
2024-01-19 11:33:31 -0700

    Add dynamic values to App template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 
 function App() {
+    const title = "Welcome to my new blog";
+    const likes = 101;
+    const link = "https://tecnate.dev";
+
     return (
         <div className="App">
             <div className="content">
-                <h1>App Component</h1>
+                <h1>{title}</h1>
+                <p>{likes} readers like this.</p>
+                <a href={link} target="blank">
+                    Tecnate
+                </a>
             </div>
         </div>
     );
