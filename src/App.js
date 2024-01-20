6d593de4a14554dd5357a2c68c18e096d0e2b1de
Nathan Smith <nathanielvs@gmail.com>
2024-01-19 19:43:06 -0700

    Add global styles and inline Navbar styles, remove App styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import "./App.css";
 import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
-    const title = "Welcome to my new blog";
-    const likes = 101;
-    const link = "https://tecnate.dev";
-
     return (
         <div className="App">
             <Navbar />
             <div className="content">
                 <Home />
-                <p>{likes} readers like this.</p>
-                <a href={link} target="blank">
-                    Tecnate
-                </a>
             </div>
         </div>
     );
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,19 @@
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>The Cyborg Gazette</h1>
+            <h1>The Justice League Standard</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a
+                    href="/create"
+                    style={{
+                        color: "white",
+                        backgroundColor: "#f1356d",
+                        borderRadius: "8px",
+                    }}
+                >
+                    New Blog
+                </a>
             </div>
         </nav>
     );
