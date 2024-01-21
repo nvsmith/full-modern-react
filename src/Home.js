35a7e1c82f5d1e93c878281d3a5c5127652741c0
Nathan Smith <nathanielvs@gmail.com>
2024-01-20 21:56:51 -0700

    Add click events for a function reference and an anon function

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log("Hello world", e);
+    };
+
+    const handleClickAgain = (name, e) => {
+        console.log(`Hello ${name}`, e.target);
+    };
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            {/* Pass a reference to a function without invoking it */}
+            <button onClick={handleClick}>Click Me</button>
+            {/* Pass an argument using an anonymous function */}
+            <button onClick={(e) => handleClickAgain("Superman", e)}>
+                Click Me Again
+            </button>
         </div>
     );
 };
