afc8c56be18fd5ea7bd3b1b775a7f25702d4e260
Nathan Smith <nathanielvs@gmail.com>
2024-01-22 18:41:29 -0700

    Output blog titles & authors from list and style each entry

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,35 @@ import { useState } from "react";
 
 const Home = () => {
     // [initial value, function to rerender component]
-    const [name, setName] = useState("Superman");
-    const [age, setAge] = useState(33);
-
-    const handleClick = () => {
-        setName("Batman");
-        setAge(32);
-    };
+    const [blogs, setBlogs] = useState([
+        {
+            title: "Superman's Blog",
+            body: "Lorem ipsum dolor",
+            author: "Superman",
+            id: 1,
+        },
+        {
+            title: "Batman's Blog",
+            body: "Ipsum ipsum dolor",
+            author: "Batman",
+            id: 2,
+        },
+        {
+            title: "Wonder Woman's Blog",
+            body: "Dolor ipsum dolor",
+            author: "Wonder Woman",
+            id: 3,
+        },
+    ]);
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>President: {name}</p>
-            <p>Age: {age} years old.</p>
-            <button onClick={handleClick}>Click Me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 };
