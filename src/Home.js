977f46b17ecf6f9bc06ae6b14213f64e655f8e11
Nathan Smith <nathanielvs@gmail.com>
2024-01-25 18:40:04 -0700

    Add loading message with conditional logic while data fetch is pending.

--- src/Home.js
+++ src/Home.js
@@ -7,22 +7,30 @@ const Home = () => {
     // set initial state to null until after posts are fetched from db
     const [blogs, setBlogs] = useState(null);
 
+    // Set initial pending state message
+    const [isPending, setIsPending] = useState(true);
+
     // Fetch data only once (when component first renders by using empty [])
     useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                // console.log(data);
-                setBlogs(data);
-            });
+        // Timeout to view pending state during testing; remove for production
+        setTimeout(() => {
+            fetch("http://localhost:8000/blogs")
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    // console.log(data);
+                    setBlogs(data);
+                    setIsPending(false);
+                });
+        }, 1000);
     }, []);
 
     return (
         <div className="home">
             {/* Props for blogs data & delete functionality used in BlogList component */}
-            {/* && logic: don't output BlogList until 'blogs=true' (once data is fetched) */}
+            {/* Conditional Template: if the left of && = true, output the right (e.g. once data is fetched) */}
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
