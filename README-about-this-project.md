47b734192500a19b3944351cb49a815fbba3e6ea
Nathan Smith <nathanielvs@gmail.com>
2024-01-28 00:19:19 -0700

    Add network & resource error catching to the fetch API. Update README explaining how to test for errors.

--- README-about-this-project.md
+++ README-about-this-project.md
@@ -28,3 +28,23 @@ This starts a server with endpoints to the blog data that you can query within y
     -   POST - Add a new blog
 -   `/blogs/{id}`
     -   DELETE - Delete a blog
+
+## Testing For Fetch Errors
+
+Step 19 is all about error handling for network problems or URL/endpoint (resource) issues.
+
+#### Network Error Testing
+
+1. Disconnect from json-server: `ctrl+C`
+2. Refresh the page. Any connection errors will be captured by the catch block and can be logged to the console.
+3. Reconnect to json-server after testing:
+
+```bash
+npx json-server --watch data/db.json --port 8000
+```
+
+#### Resource Error Testing
+
+1. In the fetch block, change the URL to something nonexistent, e.g. `"http://localhost:8000/blogs%%%%%"`.
+2. Refresh the page. Anything other than a successful delivery of the desired resource from the server will throw an `Error("message")` to the catch block.
+3. Reset the URL after testing, e.g. `http://localhost:8000/blogs`
--- src/Home.js
+++ src/Home.js
@@ -3,34 +3,51 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    // [initial value, function to rerender component]
+    // [initial value, function to rerender component] = useState(arg)
+
     // set initial state to null until after posts are fetched from db
     const [blogs, setBlogs] = useState(null);
-
     // Set initial pending state message
     const [isPending, setIsPending] = useState(true);
+    // Store errors for output to browser. Assume null on initial render.
+    const [error, setError] = useState(null);
 
     // Fetch data only once (when component first renders by using empty [])
     useEffect(() => {
         // Timeout to view pending state during testing; remove for production
         setTimeout(() => {
-            fetch("http://localhost:8000/blogs")
+            // nonexistent resource to test for error throws/catches
+            fetch("http://localhost:8000/blogs%%%%%")
                 .then((response) => {
+                    // determine if server sends back the correct response
+                    if (!response.ok) {
+                        // output for URL/resource errors
+                        throw Error(`Could not fetch data for that resource. Check that your URL/endpoint is correct.`);
+                    }
                     return response.json();
                 })
                 .then((data) => {
                     // console.log(data);
                     setBlogs(data);
                     setIsPending(false);
+                    setError(null);
+                })
+                .catch((err) => {
+                    // remove pending message
+                    setIsPending(false);
+                    // Log any network errors URL/resource errors
+                    // console.log(err.message);
+                    setError(err.message);
                 });
         }, 1000);
     }, []);
 
     return (
         <div className="home">
-            {/* Props for blogs data & delete functionality used in BlogList component */}
             {/* Conditional Template: if the left of && = true, output the right (e.g. once data is fetched) */}
             {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {/* Props for blogs data used in BlogList component */}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
