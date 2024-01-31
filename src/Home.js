cb29159eac8e2e771def3cf844f3680e9052eb60
Nathan Smith <nathanielvs@gmail.com>
2024-01-31 14:27:29 -0700

    Cleanup errors when fetch request is interrupted. Remove unused hooks from Home component.

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,3 @@
-// useState hook: allows for a dynamic, reactive value
-import { useState, useEffect } from "react";
-
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,3 +1,4 @@
+// useState hook: allows for a dynamic, reactive value
 import { useState, useEffect } from "react";
 
 // Custom hook to fetch data
@@ -9,9 +10,12 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        // Cleanup function stops fetch when component unmounts (to prevent state from updating)
+        const stopFetch = new AbortController();
+
         // Timeout to view pending state during testing; remove for production
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: stopFetch.signal })
                 .then((response) => {
                     // Determine if server sends back the correct response
                     if (!response.ok) {
@@ -25,10 +29,18 @@ const useFetch = (url) => {
                     setError(null);
                 })
                 .catch((err) => {
-                    setIsPending(false);
-                    setError(err.message);
+                    // Don't update the state for abort errors
+                    if (err.name === "AbortError") {
+                        console.log("fetch aborted");
+                    } else {
+                        setIsPending(false);
+                        setError(err.message);
+                    }
                 });
         }, 1000);
+
+        // Abort associated fetch
+        return () => stopFetch.abort();
     }, [url]);
 
     return { data, isPending, error };
