448da1c3505e01c58290a481281ab127b05b8f12
Nathan Smith <nathanielvs@gmail.com>
2024-02-01 08:30:02 -0700

    Add form to Create component with 2-way control. Style input form.

--- src/Create.js
+++ src/Create.js
@@ -1,8 +1,36 @@
+import { useState } from "react";
+
 // sfc
 const Create = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("Oracle");
+
     return (
         <div className="create">
             <h2>Add A New Blog</h2>
+            <form>
+                <label>Title:</label>
+                <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
+
+                <label>Blog Body:</label>
+                <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+
+                <label>Blog Author:</label>
+                <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="Superman">Superman</option>
+                    <option value="Batman">Batman</option>
+                    <option value="Wonder Woman">Wonder Woman</option>
+                    <option value="Oracle">Oracle</option>
+                </select>
+
+                <button>Add Blog</button>
+
+                {/* Output entries to document */}
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
         </div>
     );
 };
