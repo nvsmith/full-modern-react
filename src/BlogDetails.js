64d4f2ff40aad027b18fe92c21153d2cc2d3a47c
Nathan Smith <nathanielvs@gmail.com>
2024-02-03 21:52:50 -0700

    Add delete functionality for each blog post. Send user back home after deleting blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,19 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, isPending, error } = useFetch(`http://localhost:8000/blogs/${id}`);
+    const history = useHistory();
+
+    const deleteBlog = () => {
+        fetch(`http://localhost:8000/blogs/${id}`, {
+            method: "DELETE",
+        }).then(() => {
+            // Send user back to Home Route
+            history.push("/");
+        });
+    };
 
     return (
         <div className="blog-details">
@@ -15,6 +25,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={deleteBlog}>Delete This Blog</button>
                 </article>
             )}
         </div>
